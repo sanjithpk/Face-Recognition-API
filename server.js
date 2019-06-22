bc25dc931a333260fd4053da4542c233f2106edd
sanjithpk <sanjithpk06@gmail.com>
2019-06-22 21:18:08 +0530

    server complete

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const database = {
-    users: [
+    user: [
         {
             id: '123',
-            name: 'john',
+            name: 'John',
             email: 'john@gmail.com',
             password: 'cookies',
             entries: 0,
@@ -19,7 +19,7 @@ const database = {
         },
         {
             id: '124',
-            name: 'sally',
+            name: 'Sally',
             email: 'sally@gmail.com',
             password: 'jam',
             entries: 0,
@@ -29,12 +29,12 @@ const database = {
 }
 
 app.get('/', (req,res) => {
-    res.send(database.users);
+    res.send(database.user);
 })
 
 app.post('/signin', (req, res) => {
-    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
-        res.json('success');
+    if(req.body.email === database.user[0].email && req.body.password === database.user[0].password){
+        res.json(database.user[0]);
     }
     else {
         res.status(400).json('error logging in');
@@ -43,16 +43,44 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req,res) => {
     const {email, name, password} = req.body;
-    database.users.push({   
+    database.user.push({   
         id: '125',
         name: name,
         email: email,
-        password: password,
         entries: 0,
         joined: new Date()
     })
-    res.json(database.users[database.users.length-1]);
+    res.json(database.user[database.user.length-1]);
 })
+
+app.get('/profile/:id', (req, res) => {
+    const {id} = req.params;
+      let found = false;
+      database.user.forEach(user => {
+          if(user.id === id) {
+              found = true;
+              return res.json(user);
+          }
+      })
+      if(!found) {
+        res.status(400).json('error getting user');
+      }
+  })
+  
+  app.put('/image', (req, res) => {
+      const {id} = req.body;
+      let found = false;
+      database.user.forEach(user => {
+          if(user.id === id) {
+              found = true;
+              user.entries++;
+              return res.json(user.entries);
+          }
+      })
+      if(!found) {
+        res.status(400).json('error getting user');
+      }
+  })
 
 app.listen(3000, () => {
     console.log("App is running");
