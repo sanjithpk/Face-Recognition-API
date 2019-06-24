3ee78927871e0aa83e53f8fe17154cd4b580645f
sanjithpk <sanjithpk06@gmail.com>
2019-06-24 15:15:19 +0530

    removing hard coded values

--- server.js
+++ server.js
@@ -17,27 +17,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    user: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'jam',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
-
 app.get('/', (req,res) => {
     res.send(database.user);
 })
