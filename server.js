7eeca92cc82ee1322b5bdb5390a3290e4ff09c1f
sanjithpk <sanjithpk06@gmail.com>
2019-06-24 15:04:56 +0530

    completed signin part

--- server.js
+++ server.js
@@ -43,12 +43,22 @@ app.get('/', (req,res) => {
 })
 
 app.post('/signin', (req, res) => {
-    if(req.body.email === database.user[0].email && req.body.password === database.user[0].password){
-        res.json(database.user[0]);
-    }
-    else {
-        res.status(400).json('error logging in');
-    }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req,res) => {
